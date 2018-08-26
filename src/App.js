755042c4f39e07acf1422192d65034e17cf590eb
Bos Eriko <bos.eriko@gmail.com>
2018-08-26 12:11:22 +0800

    Removing unwanted styles and tags

--- src/App.js
+++ src/App.js
@@ -2,31 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import firebase from "firebase";
-
-const config = {
-  apiKey: "AIzaSyCz3laxK1vMpEpjJFXSx8UyzlH7mE4KBT0",
-  authDomain: "kuru-anime-network.firebaseapp.com",
-  databaseURL: "https://kuru-anime-network.firebaseio.com",
-  projectId: "kuru-anime-network",
-  storageBucket: "kuru-anime-network.appspot.com",
-  messagingSenderId: "195377790597"
-};
-
-firebase.initializeApp(config);
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Firebase Kuru Anime Project</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>hahaha</div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import firebase from "firebase";
+const config = {
+    apiKey: "AIzaSyCz3laxK1vMpEpjJFXSx8UyzlH7mE4KBT0",
+    authDomain: "kuru-anime-network.firebaseapp.com",
+    databaseURL: "https://kuru-anime-network.firebaseio.com",
+    projectId: "kuru-anime-network",
+    storageBucket: "kuru-anime-network.appspot.com",
+    messagingSenderId: "195377790597"
+};
+
+firebase.initializeApp(config);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
