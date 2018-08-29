01e3334e0509055528bbde6012c46d485ebed1ba
Bos Eriko <bos.eriko@gmail.com>
2018-08-29 18:33:31 +0800

    Making a login form

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,42 @@ import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  emailOnChange = (event) => {
+    this.setState({
+      email: event.target.value
+    });
+  }
+  passwordOnChange = (event) => {
+    this.setState({
+      password: event.target.value
+    });
+  }
+  submitRegistration = (event) => {
+    console.log("Email: ", this.state.email);
+    console.log("Password: ", this.state.password);
+    event.preventDefault();
+  }
   render() {
+    const { submitRegistration, emailOnChange, passwordOnChange } = this;
+    const inputStyle = "border block w-full mb-2 p-3 rounded bg-grey-lighter";
     return (
       <div>
         <Header />
-        <div>Under Maintenance</div>
+        <div className="container mx-auto py-4">
+          <h3 className="pb-2">Registration</h3>
+          <form onSubmit={submitRegistration}>
+            <input className={inputStyle} type="text" placeholder="E-mail" onChange={emailOnChange} />
+            <input className={inputStyle} type="password" placeholder="Password" onChange={passwordOnChange} />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-        <header>
-            this is the header
+        <header className="bg-grey-darker text-white block py-4 border-b border-solid border-grey-darkest shadow">
+            <div className="container mx-auto">
+                Kuru Anime
+            </div>
         </header>
     );
   }
