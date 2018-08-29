3333eaf14a55c2126edde769301a9cdeb2f0a3b9
Bos Eriko <bos.eriko@gmail.com>
2018-08-29 18:00:07 +0800

    Added a Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>hahaha</div>
+      <div>
+        <Header />
+        <div>Under Maintenance</div>
+      </div>
     );
   }
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+        <header>
+            this is the header
+        </header>
+    );
+  }
+}
+
+export default Header;
