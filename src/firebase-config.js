dc0d509e33ea6b48279af0f4000475feff11b465
Bos Eriko <bos.eriko@gmail.com>
2018-08-30 16:41:33 +0800

    tracked firebaseconfig again

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCz3laxK1vMpEpjJFXSx8UyzlH7mE4KBT0",
+    authDomain: "kuru-anime-network.firebaseapp.com",
+    databaseURL: "https://kuru-anime-network.firebaseio.com",
+    projectId: "kuru-anime-network",
+    storageBucket: "kuru-anime-network.appspot.com",
+    messagingSenderId: "195377790597"
+}
