dae820512910a9c7d17c71c79dbb372b39007c95
Bos Eriko <bos.eriko@gmail.com>
2018-08-31 15:18:26 +0800

    Added a component specially made for authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import UserAuthentication from './pages/UserAuthentication';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: ""
-    };
-  }
-  emailOnChange = (event) => {
-    this.setState({
-      email: event.target.value
-    });
-  }
-  passwordOnChange = (event) => {
-    this.setState({
-      password: event.target.value
-    });
-  }
-  submitRegistration = (event) => {
-    console.log("Email: ", this.state.email);
-    console.log("Password: ", this.state.password);
-    event.preventDefault();
-  }
   render() {
-    const { submitRegistration, emailOnChange, passwordOnChange } = this;
-    const inputStyle = "border block w-full mb-2 p-3 rounded bg-grey-lighter";
-    const buttonStyle = "rounded block w-full p-3 bg-black text-white";
     return (
       <div>
         <Header />
-        <div className="container mx-auto py-4">
-          <h3 className="pb-2">Registration</h3>
-          <form onSubmit={submitRegistration}>
-            <input className={inputStyle} type="text" placeholder="E-mail" onChange={emailOnChange} />
-            <input className={inputStyle} type="password" placeholder="Password" onChange={passwordOnChange} />
-            <button className={buttonStyle} type="submit">Submit</button>
-          </form>
-        </div>
+        <UserAuthentication />
       </div>
     );
   }
---
+++ src/pages/UserAuthentication.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class UserAuthentication extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  emailOnChange = (event) => {
+    this.setState({
+      email: event.target.value
+    });
+  }
+  passwordOnChange = (event) => {
+    this.setState({
+      password: event.target.value
+    });
+  }
+  submitRegistration = (event) => {
+    console.log("Email: ", this.state.email);
+    console.log("Password: ", this.state.password);
+    event.preventDefault();
+  }
+  render() {
+    const { submitRegistration, emailOnChange, passwordOnChange } = this;
+    const inputStyle = "border block w-full mb-2 p-3 rounded bg-grey-lighter";
+    const buttonStyle = "rounded block w-full p-3 bg-black text-white";
+    return (
+      <div>
+        <div className="container mx-auto py-4">
+          <h3 className="pb-2">Registration</h3>
+          <form onSubmit={submitRegistration}>
+            <input className={inputStyle} type="text" placeholder="E-mail" onChange={emailOnChange} />
+            <input className={inputStyle} type="password" placeholder="Password" onChange={passwordOnChange} />
+            <button className={buttonStyle} type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserAuthentication;
