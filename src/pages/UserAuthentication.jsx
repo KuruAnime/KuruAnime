8cca67f9325c5b9c7105de4a989bb993c03ee005
Bos Eriko <bos.eriko@gmail.com>
2018-08-31 15:30:05 +0800

    added a checkbox to check if user is registering or loging in

--- src/pages/UserAuthentication.jsx
+++ src/pages/UserAuthentication.jsx
@@ -5,7 +5,8 @@ class UserAuthentication extends Component {
     super(props);
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      pageStatus: "Login"
     };
   }
   emailOnChange = (event) => {
@@ -23,17 +24,30 @@ class UserAuthentication extends Component {
     console.log("Password: ", this.state.password);
     event.preventDefault();
   }
+  registerCheckbox = (event) => {
+    if(event.target.checked) {
+      this.setState({
+        pageStatus: "Register"
+      });
+    }else{
+      this.setState({
+        pageStatus: "Login"
+      });
+    }
+  }
   render() {
-    const { submitRegistration, emailOnChange, passwordOnChange } = this;
+    const { submitRegistration, emailOnChange, passwordOnChange, registerCheckbox } = this;
+    const { pageStatus } = this.state;
     const inputStyle = "border block w-full mb-2 p-3 rounded bg-grey-lighter";
     const buttonStyle = "rounded block w-full p-3 bg-black text-white";
     return (
       <div>
         <div className="container mx-auto py-4">
-          <h3 className="pb-2">Registration</h3>
+          <h3 className="pb-2">{pageStatus}</h3>
           <form onSubmit={submitRegistration}>
             <input className={inputStyle} type="text" placeholder="E-mail" onChange={emailOnChange} />
             <input className={inputStyle} type="password" placeholder="Password" onChange={passwordOnChange} />
+            <label className="mb-2 block text-right" ><input onChange={registerCheckbox} type="checkbox" name="register" id="registerCheckbox"/> Register</label>
             <button className={buttonStyle} type="submit">Submit</button>
           </form>
         </div>
