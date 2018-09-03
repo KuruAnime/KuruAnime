020da438c40148b0c989f407004546ea26137daf
Bos Eriko <bos.eriko@gmail.com>
2018-09-03 18:11:51 +0800

    Added a login and register option

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -4,7 +4,7 @@ import firebase from "firebase/app";
 import 'firebase/auth';
 import { firebaseConfig } from "../firebase-config";
 
-firebase.initializeApp( firebaseConfig );
+firebase.initializeApp(firebaseConfig);
 
 class AuthPage extends Component {
     constructor(props) {
@@ -12,6 +12,7 @@ class AuthPage extends Component {
         this.state = {
             email: "",
             password: "",
+            passwordCheck: "",
             registerStatus: false
         };
     }
@@ -25,9 +26,30 @@ class AuthPage extends Component {
             password: event.target.value
         });
     }
+    passwordCheckOnChange = (event) => {
+        this.setState({
+            passwordCheck: event.target.value
+        });
+    }
     submitRegistration = (event) => {
-        console.log("Email: ", this.state.email);
-        console.log("Password: ", this.state.password);
+        const { email, password, passwordCheck, registerStatus } = this.state;
+        if (registerStatus) {
+            if (password === passwordCheck) {
+                firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    console.log(errorCode, errorMessage);
+                });
+            } else {
+                console.log("Password does not match!");
+            }
+        } else {
+            firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            });
+        }
         event.preventDefault();
     }
     registerCheckbox = (event) => {
@@ -42,7 +64,7 @@ class AuthPage extends Component {
         }
     }
     render() {
-        const { submitRegistration, emailOnChange, passwordOnChange, registerCheckbox } = this;
+        const { submitRegistration, emailOnChange, passwordOnChange, registerCheckbox, passwordCheckOnChange } = this;
         const { registerStatus } = this.state;
         const inputStyle = "border block w-full mb-2 p-3 rounded bg-grey-lighter";
         const buttonStyle = "rounded block w-full p-3 bg-black text-white";
@@ -53,6 +75,7 @@ class AuthPage extends Component {
                     <form onSubmit={submitRegistration}>
                         <input className={inputStyle} type="text" placeholder="E-mail" onChange={emailOnChange} />
                         <input className={inputStyle} type="password" placeholder="Password" onChange={passwordOnChange} />
+                        {registerStatus && <input className={inputStyle} type="password" placeholder="Password Again" onChange={passwordCheckOnChange} />}
                         <label className="mb-2 block text-right" ><input onChange={registerCheckbox} type="checkbox" name="register" id="registerCheckbox" /> Register</label>
                         <button className={buttonStyle} type="submit">{registerStatus ? "Register" : "Login"}</button>
                     </form>
