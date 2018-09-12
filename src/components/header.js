81caa5c532dbfdd4fd0504a5d59cc30e5cb4639f
Bos Eriko <bos.eriko@gmail.com>
2018-09-12 19:05:38 +0800

    Added a border

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <header className="bg-kuru bg-grey-darker text-white block py-2">
+            <header className="bg-kuru bg-grey-darker text-white block border-b border-solid border-red-darker py-2">
                 <div className="container mx-auto flex items-center">
                     <div className="logo-typography text-4xl">K-Anime</div>
-                    <div className="ml-4">Social Icons Here</div>
+                    <div className="ml-4">Menu Here</div>
+                    <div className="ml-4">Search Box Here</div>
                     <div className="ml-auto">Login Field Here</div>
                 </div>
             </header>
