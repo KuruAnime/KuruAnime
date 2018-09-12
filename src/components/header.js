910103c4820c2897c5ccc14669f86de39a0bc8eb
Bos Eriko <bos.eriko@gmail.com>
2018-09-12 18:29:44 +0800

    Updated the header

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <header className="bg-grey-darker text-white block py-4 border-b border-solid border-grey-darkest shadow">
-                <div className="container mx-auto">
-                    Kuru Anime
+            <header className="bg-kuru bg-grey-darker text-white block py-2">
+                <div className="container mx-auto flex items-center">
+                    <div className="logo-typography text-4xl">K-Anime</div>
+                    <div className="ml-4">Social Icons Here</div>
+                    <div className="ml-auto">Login Field Here</div>
                 </div>
             </header>
         );
