188780923a58bb13ee2d953c85abbb6c3f8a9493
Bos Eriko <bos.eriko@gmail.com>
2018-09-16 12:24:34 +0800

    Changed the font of the logo

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ class Header extends Component {
         return (
             <header className="bg-kuru bg-grey-darker text-white block border-b border-solid border-red-darker py-2">
                 <div className="container mx-auto flex items-center">
-                    <div className="logo-typography text-4xl">K-Anime</div>
+                    <div className="logo-typography text-2xl">Kuru Anime</div>
                     <div className="ml-4">Menu Here</div>
                     <div className="ml-4">Search Box Here</div>
                     <div className="ml-auto">Login Field Here</div>
