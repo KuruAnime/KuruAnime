f24a02cfb0a4001c24d616d2496031a50d9098b7
Bos Eriko <bos.eriko@gmail.com>
2018-09-16 12:47:25 +0800

    Updated how I handle the styles

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,56 @@ import React, { Component } from 'react';
 
 class Header extends Component {
     render() {
+        const style = {
+            header: (`
+                bg-kuru
+                bg-grey-darker
+                text-white
+                block
+                border-b
+                border-solid
+                border-red-darker
+                py-2
+            `),
+            container: (`
+                container
+                mx-auto
+                flex
+                items-center
+            `),
+            logo: (`
+                logo-typography
+                text-2xl
+                no-underline
+                text-white
+            `),
+            menu: (`
+                ml-10
+                list-reset
+                flex
+            `),
+            menuItems: (`
+                text-white
+                hover:text-grey
+                no-underline
+            `),
+        };
         return (
-            <header className="bg-kuru bg-grey-darker text-white block border-b border-solid border-red-darker py-2">
-                <div className="container mx-auto flex items-center">
-                    <div className="logo-typography text-2xl">Kuru Anime</div>
-                    <div className="ml-4">Menu Here</div>
-                    <div className="ml-4">Search Box Here</div>
+            <header className={style.header}>
+                <div className={style.container}>
+                    <a href="#" className={style.logo}>Kuru Anime</a>
+                    <ul className={style.menu}>
+                        <li className="mr-4">
+                            <a className={style.menuItems} href="#">Home</a>
+                        </li>
+                        <li className="mr-4">
+                            <a className={style.menuItems} href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a className={style.menuItems} href="https://blog.kuru-anime.com/" target="_blank">Blog</a>
+                        </li>
+                    </ul>
+                    <div className="ml-10">Search Box Here</div>
                     <div className="ml-auto">Login Field Here</div>
                 </div>
             </header>
