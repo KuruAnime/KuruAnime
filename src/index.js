df074d033188e14731422eeee2b250950c524ee1
Bos Eriko <bos.eriko@gmail.com>
2018-09-19 19:27:14 +0800

    Moved firebase to index.js and cleaned app.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './layouts/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import firebase from "firebase/app";
+import 'firebase/auth';
+import { firebaseConfig } from "./firebase-config";
+
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,30 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Header from '../components/header';
-import Navigation from '../components/navigation';
 import Footer from '../components/footer';
 
-import AuthPage from '../pages/auth';
-import HomePage from '../pages/homepage';
-
 class App extends Component {
-    PageContent = () => {
-        return (
-            <Router>
-                <div>
-                    <Navigation />
-                    <Route exact path="/" component={HomePage} />
-                    <Route exact path="/auth" component={AuthPage} />
-                </div>
-            </Router>
-        );
-    }
     render() {
-        const { PageContent } = this;
         return (
             <div>
                 <Header />
-                <PageContent />
+                <div>Under Maintenance</div>
                 <Footer />
             </div>
         );
