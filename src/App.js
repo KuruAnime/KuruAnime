42b14a8217010801b0910d11cf1668af0d4710dc
BosEriko <bos.eriko@gmail.com>
2018-12-24 11:38:33 +0800

    Added React Helmet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import {Helmet} from "react-helmet";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Under Maintenance</h1>
+        <Helmet>
+          <title>Kuru Anime is Under Maintenance</title>
+        </Helmet>
+        <h1 style={{ textAlign: "center" }}>Under Maintenance</h1>
       </div>
     );
   }
