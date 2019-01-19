69d8a9da754dfeb2e5690f8bbda311094950ff47
BosEriko <bos.eriko@gmail.com>
2019-01-19 13:30:00 +0800

    Commented firebase out

--- src/view/App.js
+++ src/view/App.js
@@ -6,14 +6,14 @@ import Index from './pages/Index'
 import About from './pages/About'
 import './App.css'
 
-const config = {
-  apiKey: "AIzaSyCz3laxK1vMpEpjJFXSx8UyzlH7mE4KBT0",
-  authDomain: "kuru-anime-network.firebaseapp.com",
-  databaseURL: "https://kuru-anime-network.firebaseio.com",
-  projectId: "kuru-anime-network",
-  storageBucket: "kuru-anime-network.appspot.com",
-  messagingSenderId: "195377790597"
-};
+// const config = {
+//   apiKey: "AIzaSyCz3laxK1vMpEpjJFXSx8UyzlH7mE4KBT0",
+//   authDomain: "kuru-anime-network.firebaseapp.com",
+//   databaseURL: "https://kuru-anime-network.firebaseio.com",
+//   projectId: "kuru-anime-network",
+//   storageBucket: "kuru-anime-network.appspot.com",
+//   messagingSenderId: "195377790597"
+// };
 // firebase.initializeApp(config);
 
 class App extends Component {
