812b28521325cb178bbff023342f87bfec6e2f00
BosEriko <bos.eriko@gmail.com>
2019-03-07 18:22:01 +0800

    Empty vue

---
+++ src/components/Homepage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Homepage',
+  data() {
+    return {
+      msg: 'Welcome to Kuru Anime!',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
